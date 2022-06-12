ac74cfaeeb07152db64d0bbb867e887a8c2fca9b
paola390 <75397487+paola390@users.noreply.github.com>
2022-06-11 22:06:20 -0300

    add PassengerManager

--- src/com/br/elevator/controle/system/debug/PassengerManager.java
+++ src/com/br/elevator/controle/system/debug/PassengerManager.java
@@ -1,45 +1,45 @@
 package com.br.elevator.controle.system.debug;
 
+import java.awt.Graphics;
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+import com.br.EaseGameEngine.Vector2;
 import com.br.elevator.controle.system.Building;
 
-public class PassengerManager extends Thread {
-	public static Passengers[] passengers;
-	public static final int n = 40;
-	private int nOffset;
-	
-	public PassengerManager(int nOffset) {
-		this.nOffset = nOffset;
-	}
-	
-	@Override
-	public void run() {
-		super.run();
-		float deltaT = 0;
-		while (!Building.quit) {
-			long start = System.currentTimeMillis();
+import java.awt.Canvas;
 
-			Input();
+public class PassengerManager {
+	public Passengers[] passengers;
+	public static final int n = 11;
+	private Random r;
+	public static Semaphore sem;
 
-			Update(deltaT);
+	public PassengerManager(Graphics g, Canvas c) {
+		sem = new Semaphore(1);
+		this.r = new Random();
+		passengers = new Passengers[n];
+		
+		Vector2 size = new Vector2(0, 0, 10, 25);
+		float y = 0;
+		float x = Building.floorSize;
+		for (int i = 0; i < passengers.length; i++) {
+			if (this.r.nextInt(2) == 0) {
+				float p = (Building.sizePlataform - Building.floorSize) / size.width;
+				x = p * (this.r.nextInt((int) size.width) + 1) + Building.floorSize - size.width;
+			} else {
 
-			Render();
-
-			long end = System.currentTimeMillis();
-			deltaT = (float) (end - start) / 1000.0f;
+				float p = (Building.sizePlataform - Building.floorSize) / size.width;
+				x = c.getWidth() - Building.sizePlataform + p * this.r.nextInt((int) size.width) + Building.floorSize
+						- size.width;
+			}
+			int factor = this.r.nextInt(Building.F);
+			y = Building.hightWall * factor + Building.floorSize * factor + Building.floorSize + Building.hightWall
+					- size.height;
+			// y = Building.hightWall * factor + size.height;
+			Vector2 position = new Vector2(x, y);
+			passengers[i] = new Passengers(size, position, Building.F - factor, g, c);
+			passengers[i].start();
 		}
-	}
-	
-	private void Input() {
-
-	}
-
-	private void Update(float delta) {
-		for(int i = 0; i + nOffset < passengers.length; i += nOffset) {
-			//nesse ponto estou acessando os passageros em threads especificos
-		}
-	}
-
-	private void Render() {
-
 	}
 }
